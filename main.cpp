d527abcb58b654b8d84dfcab4824f1fc0bd4a024
126Florian <91480650+126Florian@users.noreply.github.com>
2021-11-13 21:06:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,21 @@
 using namespace std;
 
 int main() {
-    int n=0, a=0, b=1, c=a+b;
+    int n=0; 
     cin>>n;
-    cout<<1<<endl;
-    for(int i=0; i<(n-1); i++){
+    if(n<2){
+    cout<< "errore" <<endl;
+    }else{
+        cout<<1<<endl;
+        int a=0, b=1, c=a+b;
+        for(int i=0; i<(n-1); i++){
         c=a+b;
         a=b;
         b=c;
         cout<<c<<endl;
     }
+    }
+    
+    
     return 0;
 }
